57c5d44200074bff097a5059753891a6f7579343
MEGAFRAN <61279998+MEGAFRAN@users.noreply.github.com>
2022-04-23 00:31:14 +0000

    Deploying to gh-pages from @ MEGAFRAN/crypto-crea@455fc5679d11b55a4a049f796bbd8ce970fa871e 🚀

--- _next/static/vGlJuIXzP9m7IL6PyvKy_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0ce5a4c56e42ccff.css","static/chunks/pages/index-b2b46fd1eb346b02.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
